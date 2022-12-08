99168618122f2ba60658a143935c4fbccc107980
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-12-08 16:09:54 -0300

    feat: alterando opt

--- System/System.cpp
+++ System/System.cpp
@@ -1,45 +1,49 @@
 #include "System.hpp"
 #include "../ContratoCtrl/ContratoCtrl.hpp"
-#include  <iostream>
+#include <iostream>
 
-
-void System::BootSystem(){
+void System::BootSystem()
+{
     bool systemLoad = true;
 
-    while(systemLoad){
+    while (systemLoad)
+    {
         std::cout << "\n\nBem Vindo\n";
-        std::cout <<  "O que voce deseja?\n";
-        std::cout <<  "A. Matricular Aluno\n";
-        std::cout <<  "B. Criar Turma\n";
-        std::cout <<  "C. Contratar Funcionario\n";
-        std::cout <<  "D. Sair\n";
+        std::cout << "O que voce deseja?\n";
+        std::cout << "1. Matricular Aluno\n";
+        std::cout << "2. Criar Turma\n";
+        std::cout << "3. Contratar Funcionario\n";
+        std::cout << "0. Sair\n";
         char opcao;
         std::cin >> opcao;
-        switch(opcao){
-            case 'a':
+        switch (opcao)
+        {
+        case '1':
             break;
-            case 'b':
+        case '2':
             break;
-            case 'c':
+        case '3':
             system("cls");
             this->IniciarContratoCtrl();
             this->FinalizarContratoCtrl();
             break;
-            case 'd':
+        case '0':
             systemLoad = false;
         }
     }
 }
 
-void System::IniciarContratoCtrl(){
+void System::IniciarContratoCtrl()
+{
     ContratoCtrl contratoCtrl;
     std::cout << "Informe o CPF do Professor a ser contratado\n";
-    int cpf;    
+    int cpf;
     std::cin >> cpf;
     contratoCtrl.IniciarContrato(cpf);
 }
 
-void System::FinalizarContratoCtrl(){
+void System::FinalizarContratoCtrl()
+{
 
     std::cout << "Deseja Finalizar o Processor? (s)sim (n)nao";
     std::cout << "Contrato Finalizado com suceso";
