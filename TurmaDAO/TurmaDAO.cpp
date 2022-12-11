1743575cb856451cdc02ccbf2392bd74f4e50997
Diogo <diogorodrigueslife@gmail.com>
2022-12-11 16:19:39 -0300

    adicionadno a turma ao contrato

--- TurmaDAO/TurmaDAO.cpp
+++ TurmaDAO/TurmaDAO.cpp
@@ -1,10 +1,22 @@
 #include "TurmaDAO.hpp"
 
+TurmaDAO::TurmaDAO(){
+    this->countTurmas = 0;
+}
+
 Turma *TurmaDAO::Create(){
     Turma *turma = new Turma;
-    /* Adicionar turma ao vetor de turma */
+    Modalidade *modalidade = new Modalidade;
+    Professor *professor = new Professor;
+    turma->SetModalidade(modalidade);
+    turma->SetProfessor(professor);
+    turma->SetDataInicio("22032022");
+    turma->SetDataTermino("22032024");
+    this->turmas[this->countTurmas++] = turma;
+
     return turma;
 }
+
 Turma* TurmaDAO::Retrive(char*){
     /* buscar turma no vetor e retornar a tuma com a nome da modalidade recebida por parametro */
     return this->turmas[0];
--- TurmaDAO/TurmaDAO.hpp
+++ TurmaDAO/TurmaDAO.hpp
@@ -3,8 +3,10 @@
 
 class TurmaDAO{
     Turma *turmas[10];
+    int countTurmas;
 
 public:
+    TurmaDAO();
     Turma *Create();
     Turma *Retrive(char *);
     void Delete();
