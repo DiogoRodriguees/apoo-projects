c19c2bf88bba6c08f6681aaeb62902db4d88ee5e
Diogo <diogorodrigueslife@gmail.com>
2022-12-11 09:57:23 -0300

    alterando ihc da confirmacao

--- ContratoCtrl/ContratoCtrl.cpp
+++ ContratoCtrl/ContratoCtrl.cpp
@@ -37,10 +37,11 @@ void ContratoCtrl::InserirModalidadesDoProfessor(Modalidade* modalidade){
 
 void ContratoCtrl::Confirmar(){
     std::cout << "Informacoes do Contrato\n";
-    std::cout << "Data de Inicio : " << this->contrato->GetPeriodoDeInicio() << "\n";
-    std::cout << "Data de Termino: " << this->contrato->GetPeriodoDeTermino() << "\n";
-    std::cout << "\nModalidade: " << this->contrato->GetModalidade()->GetNome()  << "\n";
-    std::cout << "Nome do Professor: " << this->contrato->GetProfessor()->GetNome()  << "\n";
+    std::cout << "* Data de Inicio : " << this->contrato->GetPeriodoDeInicio() << "\n";
+    std::cout << "* Data de Termino: " << this->contrato->GetPeriodoDeTermino() << "\n";
+    std::cout << "* Data de Recisao: " << "\n";
+    std::cout << "* Modalidade.....: " << this->contrato->GetModalidade()->GetNome()  << "\n";
+    std::cout << "* Nome do Professor: " << this->contrato->GetProfessor()->GetNome()  << "\n";
     std::cout << "\nConfirmar informacoes do contrato? (s)sim (n)nao ";
     
     char confirma;
